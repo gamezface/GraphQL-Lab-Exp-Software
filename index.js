1a109b0265cb6c4e15db19466a5088a6c6ad556e
Anderson Barbosa <anderson.barbosa@rarolabs.com.br>
2020-02-19 16:23:27 -0300

    fix 502 error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs')
 
-axios.defaults.headers.common['Authorization'] = 'Bearer 4461c4ad95016193315227ea1b7b59b3b98cd7aa';
+axios.defaults.headers.common['Authorization'] = 'Bearer f9836e57fd3b7c382754cf6c4f1765c78495b18f';
 
 var after = ""
 
@@ -11,21 +11,26 @@ const limit = 1000
 
 const getData = async () => {
     try {
-        const query = `query example{ search (query: "stars:>10", type: REPOSITORY, first:20${after}) { pageInfo { hasNextPage endCursor } nodes { ... on Repository { nameWithOwner createdAt pullRequests(states: MERGED) { totalCount } stargazers { totalCount } releases { totalCount } updatedAt primaryLanguage { name } closedIssues: issues(states:CLOSED) { totalCount } totalIssues: issues { totalCount } } } } }`
+        const query = `query ExLab1 {
+            search(query: "stars:>100", type: REPOSITORY, first: 100${after}) {
+              nodes {
+                ... on Repository {
+                  nameWithOwner
+                  primaryLanguage {
+                    id: id
+                    name: name
+                  }
+                }
+              }
+            }
+          }`;
+        //   `query example{ search (query: "stars:>10", type: REPOSITORY, first:20${after}) { pageInfo { hasNextPage endCursor } nodes { ... on Repository { nameWithOwner createdAt pullRequests(states: MERGED) { totalCount } stargazers { totalCount } releases { totalCount } updatedAt primaryLanguage { name } closedIssues: issues(states:CLOSED) { totalCount } totalIssues: issues { totalCount } } } } }`
         const result = await axios.post('https://api.github.com/graphql', {
             query: query
         })
         const data = result.data.data.search
-        endCursor = data.pageInfo.endCursor
-        if (data.pageInfo.hasNextPage && current <= limit) {
-            fullData = fullData.concat(data.nodes)
-            after = `, after: "${endCursor}"`
-            current += 20
-            console.log(fullData.length)
-            await getData()
-        } else {
-            fs.writeFile('data.json', JSON.stringify(fullData), err => console.log(err))
-        }
+        console.log(data)
+        fs.writeFile('data.json', JSON.stringify(data), err => console.log(err))
     } catch (e) {
         console.log(e)
     }
