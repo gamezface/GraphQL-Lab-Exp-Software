e7c8b2939c6dee245524642bc1b59f50ab8d799a
Anderson Barbosa <anderson.barbosa@rarolabs.com.br>
2020-03-05 09:57:30 -0300

    sprint 2

--- index.js
+++ index.js
@@ -1,33 +1,79 @@
 const axios = require('axios');
 const fs = require('fs')
+const Json2Csv = require("json2csv").parse;
+var cursor = 0
+var after = ""
+const limit = 200;
+var resultList = []
 
 axios.defaults.headers.common['Authorization'] = 'Bearer ';
 
-var after = ""
 const getData = async () => {
   try {
-    const query = `query ExLab1 {
-            search(query: "stars:>100", type: REPOSITORY, first: 100${after}) {
-              nodes {
-                ... on Repository {
-                  nameWithOwner
-                  primaryLanguage {
-                    id: id
-                    name: name
-                  }
-                }
-              }
+    const query = `query RepositoriosPopulares {
+      search(query: "stars:>100", type: REPOSITORY, first:20${after}) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        nodes {
+          ... on Repository {
+            nameWithOwner
+            createdAt
+            pullRequests (states: MERGED){
+              totalCount
             }
-          }`;
+            releases {
+              totalCount
+            }
+            updatedAt
+            primaryLanguage {
+              name
+            }
+            closedIssues: issues(states: CLOSED) {
+              totalCount
+            }
+            totalIssues: issues {
+              totalCount
+            }
+            stargazers {
+              totalCount
+            }
+          }
+        }
+      }
+    }`;
     const result = await axios.post('https://api.github.com/graphql', {
       query: query
     })
     const data = result.data.data.search
-    console.log(data)
-    fs.writeFile('data.json', JSON.stringify(data), err => console.log(err))
+    if (data.pageInfo.hasNextPage || cursor < limit) {
+      after = `, after: "${data.pageInfo.endCursor}"`;
+      resultList = resultList.concat(data.nodes);
+      cursor += 20;
+      await getData();
+    } else {
+      writeFile(resultList);
+    }
   } catch (e) {
-    console.log(e)
+    !!e.response && e.response.status == 502 ? await getData() : console.log(e);
   }
+}
+const writeFile = (data) => {
+  const csv = Json2Csv(data, {
+    fields: [
+      "nameWithOwner",
+      "createdAt",
+      "pullRequests",
+      "releases",
+      "updatedAt",
+      "primaryLanguage",
+      "closedIssues",
+      "totalIssues",
+      "stargazers"
+    ]
+  });
+  fs.writeFileSync("data.csv", csv);
 }
 
 getData()
